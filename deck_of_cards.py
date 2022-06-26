4b85a9dca367041d4a4683b88e93249c41560a3c
diego-espindola <diegoespindola9404@gmail.com>
2022-06-26 12:04:55 -0300

    Created the class Deck

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,4 +1,5 @@
 # This is a sample Python script about OOP.
+import random
 
 class Card(object):
     def __init__(self, suit, value):
@@ -11,7 +12,25 @@ class Card(object):
 
 class Deck(object):
     def __init__(self):
-        pass
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s, v))
+
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            rand = random.randint(0, i)
+            self.cards[i], self.cards[rand] = self.cards[rand], self.cards[i]
+
+    def draw(self):
+        return self.cards.pop()
 
 
 class Player(object):
@@ -19,6 +38,12 @@ class Player(object):
         pass
 
 
+# card = Card("Clubs", 6)
+# card.show()
 
-car = Card("Clubs", 6)
-car.show()
+deck = Deck()
+deck.shuffle()
+deck.show()
+
+card = deck.draw()
+card.show()
