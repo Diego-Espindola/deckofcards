53475065c355fe91d96f1c688bf1363a1ce8c074
diego-espindola <diegoespindola9404@gmail.com>
2022-06-26 12:29:50 -0300

    Created the class Player and tested drawing 11 cards of the deck to a hand

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,6 +1,7 @@
 # This is a sample Python script about OOP.
 import random
 
+
 class Card(object):
     def __init__(self, suit, value):
         self.value = value
@@ -29,21 +30,33 @@ class Deck(object):
             rand = random.randint(0, i)
             self.cards[i], self.cards[rand] = self.cards[rand], self.cards[i]
 
-    def draw(self):
+    def draw_card(self):
         return self.cards.pop()
 
 
 class Player(object):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
 
+    def draw(self, d):
+        self.hand.append(d.draw_card())
+        return self.hand
 
-# card = Card("Clubs", 6)
-# card.show()
+    def show_hand(self):
+        for c in self.hand:
+            c.show()
+
 
 deck = Deck()
 deck.shuffle()
-deck.show()
 
-card = deck.draw()
-card.show()
+diego = Player("Diego")
+cards_in_hand = 11
+for x in range(0, cards_in_hand):
+    diego.draw(deck)
+hand = []
+for card in diego.hand:
+    hand.append("{} of {}".format(card.value, card.suit))
+
+print(hand)
