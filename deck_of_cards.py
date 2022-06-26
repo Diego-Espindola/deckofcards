70962764bd93aeaf78d6fdb14c04441fc5352967
diego-espindola <diegoespindola9404@gmail.com>
2022-06-26 11:18:58 -0300

    Created the class Card

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,8 +1,12 @@
 # This is a sample Python script about OOP.
 
 class Card(object):
-    def __init__(self):
-        pass
+    def __init__(self, suit, value):
+        self.value = value
+        self.suit = suit
+
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
 
 
 class Deck(object):
@@ -15,3 +19,6 @@ class Player(object):
         pass
 
 
+
+car = Card("Clubs", 6)
+car.show()
