e24d9ea827811c3565106c217b62a6bd393b4448
diego-espindola <diegoespindola9404@gmail.com>
2022-06-26 10:33:10 -0300

    Creating repo

---
+++ deck_of_cards.py
@@ -0,0 +1,17 @@
+# This is a sample Python script about OOP.
+
+class Card(object):
+    def __init__(self):
+        pass
+
+
+class Deck(object):
+    def __init__(self):
+        pass
+
+
+class Player(object):
+    def __init__(self):
+        pass
+
+
